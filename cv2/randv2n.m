db610bd96a20deb405e8b666e05ae92f30d69426
Lukas <lukasurin@gmail.com>
2013-10-13 23:22:24 +0200

    Generovanie
    Funkcia rand2n vygeneruje vektor s dvoma shlukmi s normalnym rozdelenim.
    Data su nahodne rozostavene vo vektore. Parametre urcuju tieto zhluky.
    n1 - pocet prvkov 1. zhluku
    s1 - stredna hodnota 1. zhluku
    r1 - odchylka 1. zhluku
    n2 - pocet prvkov 2. zhluku
    s2 - stredna hodnota 2.zhluku
    r2 - odchylka 2. zhluku

---
+++ cv2/randv2n.m
@@ -0,0 +1,14 @@
+function [ X ] = randv2n( n1, s1, r1, n2, s2, r2 )
+    
+    X = zeros(1,n1+n2);
+    
+    vec1 = sqrt(r1) * rand(1,n1) + s1;
+    vec2 = sqrt(r2) * rand(1,n2) + s2;
+    
+    X(1,1:n1)= vec1;
+    X(1,n1+1:n1+n2) = vec2;
+
+    X = X(randperm(length(X)));
+    
+end
+
